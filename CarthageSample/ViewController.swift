dcafd95f65a25646111b22f97181cd93a118448d
Ivan Nosar <v-ivnosa@microsoft.com>
2019-07-05 10:12:34 +0300

    App configured

--- CarthageSample/ViewController.swift
+++ CarthageSample/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Alamofire.request("https://httpbin.org/get")
     }
 
 
